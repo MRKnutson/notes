ddda3774e218d1ab151e1b7106c99827ef9d99fe
Michael Knutson <mrknutson44@gmail.com>
2021-11-15 14:19:00 -0900

    added create feature

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,15 @@ class NotesController < ApplicationController
     render component: "Notes", props:{ notes: notes }
   end
 
+  def new
+    render component: "NewNotes"
+  end
+
+  def create
+    Note.create(title: params[:note][:title], description: params[:note][:description])
+    redirect_to notes_path
+  end
+
   
 
 end
